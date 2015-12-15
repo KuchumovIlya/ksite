a6a796c2245750b1a743bfda9fd465250d204bc6
KuchumovIlya <sdp.kuchumov-ilya@yandex.ru>
2015-12-15 21:46:56 +0500

    design

--- forum/static/forum/topic_script.js
+++ forum/static/forum/topic_script.js
@@ -1,6 +1,7 @@
 function get_url() {
     topic_id = $(".modification_div").attr("topic_id");
     url = "http://ilya.kolambda.com/forum/modification_date/" + topic_id;
+    //url = "http://127.0.0.1:8000/forum/modification_date/" + topic_id;
     return url
 }
 
